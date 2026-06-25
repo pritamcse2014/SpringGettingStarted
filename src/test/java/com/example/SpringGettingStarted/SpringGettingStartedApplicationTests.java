86a61058a60ec5fb20e4924eb6bcd051bcb53811
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2026-06-25 20:23:38 +0600

    DIUsingSpringBoot

--- src/test/java/com/example/SpringGettingStarted/SpringGettingStartedApplicationTests.java
+++ src/test/java/com/example/SpringGettingStarted/SpringGettingStartedApplicationTests.java
@@ -3,13 +3,20 @@ package com.example.SpringGettingStarted;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class SpringGettingStartedApplicationTests {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringGettingStartedApplication.class, args);
-		System.out.println("Application Started");
+		ApplicationContext applicationContext = SpringApplication.run(SpringGettingStartedApplication.class, args);
+//		System.out.println("Application Started");
+		Alien alien = new Alien();
+		alien.code();
+		Alien alien2 = applicationContext.getBean(Alien.class);
+		alien2.code();
+		Alien alien3 = applicationContext.getBean(Alien.class);
+		alien3.code();
 	}
 
 }
