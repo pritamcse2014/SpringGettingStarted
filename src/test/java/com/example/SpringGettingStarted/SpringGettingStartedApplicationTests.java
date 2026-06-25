c911e70d5944eda5a730b9bcda1e95848a521035
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2026-06-25 21:03:02 +0600

    AutowiringInSpringBoot

--- src/test/java/com/example/SpringGettingStarted/SpringGettingStartedApplicationTests.java
+++ src/test/java/com/example/SpringGettingStarted/SpringGettingStartedApplicationTests.java
@@ -13,10 +13,13 @@ class SpringGettingStartedApplicationTests {
 //		System.out.println("Application Started");
 		Alien alien = new Alien();
 		alien.code();
-		Alien alien2 = applicationContext.getBean(Alien.class);
-		alien2.code();
-		Alien alien3 = applicationContext.getBean(Alien.class);
-		alien3.code();
+//		Alien alien2 = applicationContext.getBean(Alien.class);
+//		alien2.code();
+//		Alien alien3 = applicationContext.getBean(Alien.class);
+//		alien3.code();
+
+		Laptop laptop = applicationContext.getBean(Laptop.class);
+		laptop.compile();
 	}
 
 }
